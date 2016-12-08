8c5e85aae66983b0d9c74b426f3e383cc8aba85e
Ludwing <ludwing.perez@solucionestip.com>
2016-12-08 17:07:34 -0600

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express"),
-	app = express(),
-	bodyParser = require("body-parser"),
-	methodOverride = require("method-override");
-	mongoose = require("mongoose");
+    app = express(),
+    bodyParser  = require("body-parser"),
+    methodOverride = require("method-override"),
+    http = require("http"),
+    server = http.createServer(app),
+    mongoose = require("mongoose");
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -11,11 +13,20 @@ app.use(methodOverride());
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	res.send("Hello world!");
+    res.send("Hello world!");
 });
 
 app.use(router);
 
-app.listen(3000, function() {
-	console.log("Node server running on http://localhost:3000");
+mongoose.connect('mongodb://localhost/tvshows', function (err, res) {
+    // body...
+    if (err) {
+    	console.log('ERROR: connecting to database. '+err);
+    }
+    app.listen(3000, function() {
+        console.log("Node server running on http://localhost:3000");
+    });
 });
+
+    
+
